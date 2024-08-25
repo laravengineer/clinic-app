5e57d1160056a9ec87b3397c34341a6468b71b36
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-25 10:21:19 -0300

    feat: add roles controllers

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'patient'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/doctor/dashboard', [DoctorController::class,'loadDoctorDashboard'])
+    ->middleware('doctor')
+    ->name('doctor-dashboard');
+
+Route::get('/admin/dashboard', [AdminController::class,'loadAdminDashboard'])
+    ->middleware('admin')
+    ->name('admin-dashboard');
 
 require __DIR__.'/auth.php';
