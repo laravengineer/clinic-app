3109bf0d158190f5c0eb7e7380e298bf8ab166b6
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-26 20:13:06 -0300

    feat: add speciality listing

---
+++ app/Livewire/SpecialityListingComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SpecialityListingComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.speciality-listing-component');
+    }
+}
