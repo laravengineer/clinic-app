b1fc722ab8872a10fc030c6ad07df23c9201649f
Eduardo Alexandre <laravengineer@gmail.com>
2024-08-26 21:48:40 -0300

    feat: add specialy model

--- app/Livewire/SpecialityRegister.php
+++ app/Livewire/SpecialityRegister.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Speciality;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class SpecialityRegister extends Component
 {
+    #[Validate('required|string')]
+    public $name = '';
+
+    public function save()
+    {
+        $this->validate();
+        Speciality::create(['name'=> $this->name]);
+
+        session()->flash('message','speciality created successfully');
+        return $this->redirect(route('admin-specialities'), navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.speciality-register');
